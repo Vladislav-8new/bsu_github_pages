de99ef08c4966364a1919a5bead3fee1dac5fb1f
Vlad Savinov <vladuhin2006@gmail.com>
2025-04-12 12:08:21 +0300

    update main

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-def rabbit_pairs(month, pair):
-    if month == 0:
-        return 0
-    elif month == 1:
-        return 1
-    else:
-        a, b = 1, 1
-        for i in range(2, month):
-            a, b = b, b + pair * a
-        return b
+import glob
 
-month = int(input())
-pair = int(input())
+from Bio import SeqIO
+from Bio.SeqUtils import gc_fraction as GC
 
-result = rabbit_pairs(month, pair)
-print(result)
+def gc_ratio(filename):
+    with open(filename) as handle:
+        records_dict = {}
+        for record in SeqIO.parse(handle, "fasta"):
+            dna = record.seq
+            gc_percentage = GC(dna)
+            records_dict[record.id] = round(gc_percentage, 6)
+    return records_dict
+
+
+if __name__ == '__main__':
+    fasta_files_list = glob.glob("*.fasta")
+    all_sequences_ratio = {}
+
+    for file in fasta_files_list:
+        seq = gc_ratio(file)
+        all_sequences_ratio.update(seq)
+
+
+    max_ratio = max(all_sequences_ratio.values())
+
+    for key, value in all_sequences_ratio.items():
+        if value == max_ratio:
+            print(f"{key}\n{value}")
